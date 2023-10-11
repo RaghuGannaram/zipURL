e3b2ca9042135c46df0b5db5d766b1dc6cb7f601
Raghu Gannaram <raghuvarangannaram@gmail.com>
2023-10-11 15:02:47 +0530

    Added health-check

--- server.js
+++ server.js
@@ -10,18 +10,23 @@ const hashRouter = require("./routes/hash.route");
 
 const app = express();
 
-app.use(cors({
-	origin: ["http://127.0.0.1:5173"],
-	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-	credentials: true,
-	optionsSuccessStatus: 204,
-}));
+// app.use(cors({
+// 	origin: ["http://127.0.0.1:5173"],
+// 	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// 	credentials: true,
+// 	optionsSuccessStatus: 204,
+// }));
+
 app.use(morganMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(connectToDataBase);
 
-app.use("/app", express.static("./public"));
+app.get("/health-check", (req, res) => {
+	res.status(200).json({ message: "OK" });
+});
+app.get("/app", express.static("./public"));
+
 app.use("/", hashRouter);
 app.use((req, res) => {
 	res.redirect("/app");
